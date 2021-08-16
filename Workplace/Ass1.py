d3e45e8827a85960a52a0f02ed88fded9a5c0054
shindesoham700 <shindesoham700@gmail.com>
2021-08-16 15:11:34 +0530

    Assignment

---
+++ Workplace/Ass1.py
@@ -0,0 +1,11 @@
+list=[10,20,30,40,50,60,70,80,90,100]
+
+print(list)
+
+print(list[2:5])
+
+list.reverse()
+print(list)
+
+list.append(110)
+print(list)
---
+++ Workplace/Ass2.py
@@ -0,0 +1,14 @@
+
+tup = ('a', 'e', 'i', 'o', 'i', 'u','a', 'e', 'o')
+
+print(type(tup))
+
+print(tup)
+
+count = tup.count('i')
+
+
+print('The count of i is:', count)
+
+index = tup.index('i')
+print('The index of i:', index)
